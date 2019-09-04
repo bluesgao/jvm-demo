f0490ecf16757e6e71ac95b1c414edf6423fd72e
gaoxin11 <gaoxin11@jd.com>
2019-09-04 19:55:04 +0800

    object header

---
+++ src/main/java/com/bluesgao/jvmdemo/jom/ObjectHeaderDemo.java
@@ -0,0 +1,18 @@
+package com.bluesgao.jvmdemo.jom;
+
+import org.openjdk.jol.info.ClassLayout;
+
+/**
+ * 输出对象头信息
+ * https://www.cnblogs.com/LemonFive/p/11246086.html
+ */
+public class ObjectHeaderDemo {
+    static class A {
+        boolean flag = true;
+    }
+
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(ClassLayout.parseInstance(a).toPrintable());
+    }
+}
